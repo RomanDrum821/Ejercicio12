9aaed9885376028e8dc09fc00fd642a1245c8d77
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2023-12-05 14:06:58 -0600

    first commit

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css',
+})
+export class CalculadoraComponent {
+  numero1: number = 2;
+  numero2: number = 5;
+  resultado: number = 0;
+
+  sumarNumeros() {
+    this.resultado = this.numero1 + this.numero2;
+    //alert('Suma de numeros');
+  }
+
+  restarNumeros() {
+    this.resultado = this.numero1 - this.numero2;
+    //alert('Resta de numeros');
+  }
+
+  multiplicacionNumeros() {
+    this.resultado = this.numero1 * this.numero2;
+    //alert('Multiplicar numeros');
+  }
+
+  dividirNumeros() {
+    if (this.numero2 == 0) alert('No se puede realizar la division');
+    else this.resultado = this.numero1 / this.numero2;
+    //alert('Dividir numeros');
+  }
+}
